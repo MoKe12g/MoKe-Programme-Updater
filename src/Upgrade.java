98f7028cf846da8a4b734d1dd03e56149098c285
MoKe Programme <moke12g@gmail.com>
2018-01-20 10:35:44 +0100

    First Test Release

--- src/Upgrade.java
+++ src/Upgrade.java
@@ -40,7 +40,7 @@ public class Upgrade {
     	if (result == JOptionPane.YES_OPTION) {
     		System.out.println("Benutzer Updatet das Programm");
     		saveUrl("MoKe-Programme-Updater" + Start.Programmversion, downloadURL);
-    		//Hier was einbauen um die neue Jar zu öffnen
+    		JOptionPane.showMessageDialog(null, "Bitte starten Sie um das Update zu installieren " + "MoKe-Programme-Updater" + Start.Programmversion, "Test Titel", JOptionPane.OK_OPTION);
     		System.exit(0);
             }
     	else if (result == JOptionPane.NO_OPTION) {
